1ceb92c8a8a41932e7f90540a365bbc798ab93ac
Lennart Espe <3391295+lnsp@users.noreply.github.com>
2021-12-15 21:43:05 +0100

    Fix brew test

--- Formula/valar.rb
+++ Formula/valar.rb
@@ -1,8 +1,5 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Valar < Formula
-  desc "Command line interface for valar"
+  desc "Command line interface for Valar"
   homepage "https://valar.dev"
   url "https://github.com/valar/cli/archive/v2.0.1.zip"
   version "2.0.1"
@@ -19,15 +16,6 @@ class Valar < Formula
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test cli`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    system "${bin}/valar", "version"
   end
 end
