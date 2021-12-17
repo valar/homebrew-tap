425585ce48abd614a6a44c56ea849bf496375701
Lennart Espe <3391295+lnsp@users.noreply.github.com>
2021-12-17 22:59:47 +0100

    Drop version altogether

--- Formula/valar.rb
+++ Formula/valar.rb
@@ -2,7 +2,6 @@ class Valar < Formula
   desc "Command-line interface for Valar"
   homepage "https://valar.dev"
   url "https://github.com/valar/cli/archive/v2.0.1.tar.gz"
-  version "2.0.1"
   sha256 "edbf9edb16656bd64cacc68502df9e329035f92f5c768df2adf74f8f358b1e87"
   license "Apache-2.0"
   head "https://github.com/valar/cli.git"
